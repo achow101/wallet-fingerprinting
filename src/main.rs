11ddf31d534608c8ee25b9e08ea36f66d61fc598
Andrew Chow <achow101-github@achow101.com>
2022-06-10 11:57:59 -0400

    Implement Electrum checking

--- src/main.rs
+++ src/main.rs
@@ -199,6 +199,45 @@ fn maybe_bitcoin_core(txinfo: &GetRawTransactionResult, _prevouts: &HashMap<OutP
     return true;
 }
 
+fn maybe_electrum(txinfo: &GetRawTransactionResult, prevouts: &HashMap<OutPoint, TxOut>, rpc: &Client) -> bool {
+    let tx = txinfo.transaction().unwrap();
+
+    if tx.version != 2 {
+        return false;
+    }
+
+    match classify_sequences(&tx) {
+        SequenceType::OnlyRBF => {}
+        SequenceType::OnlyNonFinal => {}
+        _ => { return false; }
+    }
+
+    if !probably_anti_fee_snipe(&tx, txinfo.confirmations, rpc) {
+        return false;
+    }
+
+    let prob_low_r = probability_low_r_grinding(&tx);
+    if prob_low_r <= 0.5 {
+        return false;
+    }
+
+    let prob_bip69 = probability_bip69(&tx);
+    match prob_bip69 {
+        Some(p) => {
+            if p > 0.5 {
+                return false;
+            }
+        }
+        None => {}
+    }
+
+    if spends_negative_ev(&tx, &prevouts) {
+        return false;
+    }
+
+    return true;
+}
+
 fn get_previous_outputs(tx: &Transaction, rpc: &Client) -> HashMap<OutPoint, TxOut> {
     let mut out = HashMap::<OutPoint, TxOut>::new();
     for txin in tx.input.iter() {
@@ -226,5 +265,12 @@ fn main() {
         println!("Maybe Bitcoin Core");
     } else {
         println!("Probably not Bitcoin COre");
+    }
+
+    let is_electrum = maybe_electrum(&txinfo, &prevouts, &rpc);
+    if is_electrum {
+        println!("Maybe Electrum");
+    } else {
+        println!("Probably not Electrum");
     }
 }
